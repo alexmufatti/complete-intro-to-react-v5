6f907ad5e3cd31b350b0f55a40e49a832db41388
Alex Mufatti <me@alexmufatti.it>
2019-07-19 16:36:04 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
+import SearchArams from "./searchParams";
+import Details from "./Details";
+import ThemeContext from "./ThemeContext";
+
+const App = () => {
+  const themeHook = useState("peru");
+  return (
+    <React.StrictMode>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt me!</Link>
+          </header>
+          <Router>
+            <SearchArams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
